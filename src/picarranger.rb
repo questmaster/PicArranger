a89687c03fa8e11da7200740c39ba3bfb6f96f4c
Daniel Jacobi <daniel@questmaster.de>
2015-09-12 00:30:25 +0200

    Initial commit

---
+++ src/picarranger.rb
@@ -0,0 +1,160 @@
+require 'exifr'
+require 'date'
+require 'optparse'
+require 'fileutils'
+require 'zlib'
+
+
+def parse_args (options)
+  args = {}
+  args[:verbose] = false
+  args[:output_folder] = File.expand_path(File.dirname(__FILE__))
+  args[:copy_only] = false
+
+  opt_parser = OptionParser.new do |opts|
+    opts.banner = "Usage: picarranger.rb [options] <imagefolders>"
+
+    opts.on("-oPATH", "--output-path=PATH", "Path where to put the processed images.") do |path|
+      args[:output_folder] = path
+    end
+
+    opts.on("-v", "--verbose", "Print verbose messages.") do
+      args[:verbose] = true
+    end
+
+    opts.on("-c", "--copy-only", "Copy only, dont move images.") do
+      args[:copy_only] = true
+    end
+
+    opts.on("-h", "--help", "Prints this help") do
+      puts opts
+      #exit
+    end
+  end
+
+  opt_parser.parse!(options)
+
+  args
+end
+
+def find_image_files (path, options)
+  files = []
+  extensions = %w(jpg jpeg nef tif tiff)
+
+  extensions.each do |extension|
+    puts "  Process extension: #{extension}..." if options[:verbose]
+
+# Sucht nach jpg's im aktuellen und darunter liegenden Verzeichnissen
+    Dir.chdir(path)
+    Dir.glob("**/*.#{extension}") do |thefile|
+      puts "    Found: #{thefile}" if options[:verbose]
+
+      files << File.expand_path(thefile, path)
+    end
+  end
+
+  files
+end
+
+def extend_and_create_path(path, folder)
+  destination_dir = File.join(path, folder)
+  Dir.mkdir(destination_dir) unless File.exists?(destination_dir)
+
+  destination_dir
+end
+
+def calc_file_crc(image)
+  File.open(image, "rb") { |f| Zlib.crc32 f.read }
+end
+
+def do_move_images_in_structure (images, options)
+  script_path = options[:output_folder]
+  verbose = options[:verbose]
+  copy_only = options[:copy_only]
+
+  Dir.chdir(script_path)
+
+  images.each do |image|
+# datum aus exif extrahieren (Jahr/Monat)
+    image_filename = File.basename(image)
+
+    case image
+      when /\.(jpg|jpeg)$/i; date_str = EXIFR::JPEG.new(image).date_time_original
+      when /\.(tif|tiff|nef)$/i; date_str = EXIFR::TIFF.new(image).date_time_original
+      else
+        raise 'No image found.'
+    end
+
+    image_year = date_str.year.to_s
+    image_month = date_str.month.to_s
+
+    destination_dir = extend_and_create_path(script_path, image_year)
+    destination_dir = extend_and_create_path(destination_dir, image_month)
+
+    destination_file = File.join(destination_dir, image_filename)
+
+    puts "  Image: #{image_filename} transfer to #{destination_file}" if verbose
+
+    crc32_source = calc_file_crc(image)
+    if not File.exists? destination_file
+
+      FileUtils.cp(image, destination_file)
+
+      if not copy_only
+        crc32_destination = calc_file_crc(destination_file)
+        if crc32_source == crc32_destination
+          puts "   File #{image_filename} moved." if verbose
+          FileUtils.rm_f(image)
+        else
+          puts "   File #{image_filename}, error at copying, reverting." if verbose
+          FileUtils.rm_f(destination_file)
+        end
+      else
+        puts "   File #{image_filename} copied." if verbose
+      end
+
+    else
+      crc32_destination = calc_file_crc(destination_file)
+      if (crc32_source == crc32_destination) and not copy_only
+        puts "   File #{image_filename} already exists. Removing source."
+        FileUtils.rm_f(image)
+      else
+        puts "   File #{image_filename} already exists. Leaving in #{File.dirname(image)}."
+      end
+    end
+  end
+end
+
+
+
+# Main code
+args  = ARGV
+
+## Parse CMD-Line commands
+options = parse_args (args)
+puts args if options[:verbose]
+
+## Check for image path
+if args.length > 0
+  args.each do |a_path|
+    path = File.expand_path(a_path)
+    if File.directory? path
+      puts "Process folder #{path}..."
+
+## Find all image files
+      image_file = find_image_files(path, options)
+      puts image_file  if options[:verbose]
+
+## Move images in Folder-Structure
+### Check integrity
+### Move file
+      do_move_images_in_structure(image_file, options)
+      
+      puts "...done."
+    end
+  end
+else
+  parse_args(["-h"])
+end
+
+
---
+++ test/picarranger_test.rb
@@ -0,0 +1,163 @@
+require_relative '../src/picarranger'
+require 'test/unit'
+require 'fileutils'
+
+class PicArrangerTest < Test::Unit::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    temp_dir = 'tmp'
+
+    if @default_folder == nil
+      @default_folder = FileUtils.pwd
+    end
+
+    FileUtils.mkdir(temp_dir)
+    FileUtils.cp(['data/B0019278.JPG', 'data/B0020882.NEF'], temp_dir)
+    FileUtils.chdir(temp_dir)
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    FileUtils.chdir(@default_folder)
+    FileUtils.rm_r('tmp')
+  end
+
+  # Test commandline options parser
+  def test_options_parser
+    input_short = '-oC:\test -v -c pass'.split(" ")
+    input_long = '--output-path=C:\test --verbose --copy-only pass'.split(" ")
+
+    [input_short, input_long].each do |input|
+      options = parse_args(input)
+
+      assert_equal('C:\test', options[:output_folder])
+      assert_true(options[:verbose])
+      assert_true(options[:copy_only])
+      assert_equal(['pass'], input)
+    end
+  end
+
+  #
+  def test_find_image_files
+    options = {}
+    options[:verbose] = false
+
+    expected = [File.expand_path('B0019278.JPG'), File.expand_path('B0020882.NEF')]
+
+    actual = find_image_files(FileUtils.pwd, options)
+
+    assert_equal(2, actual.size)
+    [1..2].each do |i|
+      assert_equal(expected[i], actual[i])
+    end
+  end
+
+  #
+  def test_extend_and_create_path
+    extend_and_create_path(FileUtils.pwd, 'test')
+
+    assert_true(File.directory?(File.expand_path('test')))
+  end
+
+  # Test file crc calculation
+  def test_calc_file_crc
+    input = File.expand_path('B0019278.jpg')
+
+    crc32 = calc_file_crc(input)
+
+    assert_equal(2586205761, crc32)
+  end
+
+  #
+  def test_do_move_images_jpg
+    options = default_options
+
+    image = File.expand_path('B0019278.jpg')
+    do_move_images_in_structure([image], options)
+
+    assert_false(File.exists?(image))
+    assert_true(File.directory?('2010'))
+    FileUtils.chdir('2010')
+    assert_true(File.directory?('3'))
+    FileUtils.chdir('3')
+    assert_true(File.exists?('B0019278.jpg'))
+  end
+
+  #
+  def test_do_move_images_tif
+    options = default_options
+
+    image = File.expand_path('B0020882.NEF')
+    do_move_images_in_structure([image], options)
+
+    assert_false(File.exists?(image))
+    assert_true(File.directory?('2011'))
+    FileUtils.chdir('2011')
+    assert_true(File.directory?('7'))
+    FileUtils.chdir('7')
+    assert_true(File.exists?('B0020882.NEF'))
+  end
+
+  #
+  def test_do_move_images_dest_exists
+    options = default_options
+    create_existing_dest_jpg
+
+    image = File.expand_path('B0019278.jpg')
+    do_move_images_in_structure([image], options)
+
+    assert_false(File.exists?(image))
+    assert_true(File.directory?('2010'))
+    FileUtils.chdir('2010')
+    assert_true(File.directory?('3'))
+    FileUtils.chdir('3')
+    assert_true(File.exists?('B0019278.jpg'))
+  end
+
+  #
+  def test_do_move_images_dest_exists_copy_only
+    options = default_options
+    create_existing_dest_jpg
+
+    image = File.expand_path('B0019278.jpg')
+    do_move_images_in_structure([image], options)
+
+    assert_true(File.exists?(image))
+    assert_true(File.directory?('2010'))
+    FileUtils.chdir('2010')
+    assert_true(File.directory?('3'))
+    FileUtils.chdir('3')
+    assert_true(File.exists?('B0019278.jpg'))
+  end
+
+  #
+  def test_do_move_images_copy_only
+    options = default_options
+    options[:copy_only] = true
+
+    fail('not implemented')
+  end
+
+  #
+  # H E L P E R
+  #
+
+  def default_options
+    options = {}
+    options[:verbose] = false
+    options[:copy_only] = false
+    options[:output_folder] = FileUtils.pwd
+
+    options
+  end
+
+  def create_existing_dest_jpg
+    new_path = '2010/3'
+    FileUtils.mkpath(new_path)
+    FileUtils.mv('B0019278.jpg', File.expand_path(new_path))
+  end
+end
